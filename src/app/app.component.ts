0aea5431dd1bd47bd332e4cab0d9367ce4676486
Nishit <nishit.parekh@rishabhsoft.com>
2017-10-27 12:52:48 +0530

    Added chart component and highcharts dependency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  chartOptions = {
+    type: 'line',
+    xAxis: 'Day',
+    yAxis: 'Tempreature'
+  };
+
 }
